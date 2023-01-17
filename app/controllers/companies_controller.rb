3b5f55445ee6bca129b2138c9ae970a2782a9312
Mahesh Reddy <mahesh@singular.dev>
2023-01-17 16:47:43 +0530

    Added the error partial to views

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -12,32 +12,51 @@ class CompaniesController < ApplicationController
     @company = Company.new
   end
 
-  def edit
-  end
+    def edit
+     respond_to do |format|
+       format.html
+       format.js
+     end
+   end
 
   def create
     @company = Company.new(company_params)
      if @company.save
-         flash[:notice] = "Company Created successfully"
-         redirect_to @company
-      else
-         render 'new', status: :unprocessable_entity
-     end
-  end
-
-  def update
-    if @company.update(company_params)
-         flash[:notice] = "Company details updated successfully"
-         redirect_to @company
-       else
-         render 'edit' , status: :unprocessable_entity
+       respond_to do |format|
+          format.html { redirect_to @company, notice: 'company was successfully Created.'}
+          format.js   { render inline: "company_reload();"}
+        end
+        else
+        render "new" , status: :unprocessable_entity
       end
-  end
+    end
+  #     render json: { message: "Company created successfully" }, status: :created
+  #       #   flash[:notice] = "Company Created successfully"
+  #        #redirect_to @company
+  #     else
+  #      render json: { errors: @company.errors } , status: :unprocessable_entity
+  #   #   render 'new', status: :unprocessable_entity
+  #    end
+  # end
+
+ def update
+    if @company.update(company_params)
+       respond_to do |format|
+       format.html { redirect_to @company, notice: 'company was successfully updated.' }
+       format.js { render inline: "company_reload();"}
+     end
+      #   flash[:notice] = "Company details updated successfully"
+      #   redirect_to @company
+     else
+        render 'edit' , status: :unprocessable_entity
+      end
+ end
 
   def destroy
     @company.destroy
-      flash[:notice] = "Company destroyed successfully"
-      redirect_to companies_path
+       render json: { message: "Company deleted successfully" }, status: :ok
+    #  flash[:notice] = "Company destroyed successfully"
+    #  redirect_to companies_path
   end
 
   private
